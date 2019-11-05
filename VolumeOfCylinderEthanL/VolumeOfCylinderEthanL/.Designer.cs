06190d1ab74ab5d7b526c8596cbe21503dd16e86
Ethan LeGault <47784129+s256202@users.noreply.github.com>
2019-11-05 13:06:55 -0500

    Added VolumeOfCylinder

---
+++ VolumeOfCylinderEthanL/VolumeOfCylinderEthanL/.Designer.cs
@@ -0,0 +1,122 @@
+﻿namespace VolumeOfCylinderEthanL
+{
+    partial class frmVolOfCylinder
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.lblRadiusText = new System.Windows.Forms.Label();
+            this.lblHeightText = new System.Windows.Forms.Label();
+            this.txtRadius = new System.Windows.Forms.TextBox();
+            this.txtHeight = new System.Windows.Forms.TextBox();
+            this.btnCalculate = new System.Windows.Forms.Button();
+            this.lblAnswer = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // lblRadiusText
+            // 
+            this.lblRadiusText.AutoSize = true;
+            this.lblRadiusText.Font = new System.Drawing.Font("Microsoft Sans Serif", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblRadiusText.Location = new System.Drawing.Point(131, 78);
+            this.lblRadiusText.Name = "lblRadiusText";
+            this.lblRadiusText.Size = new System.Drawing.Size(73, 24);
+            this.lblRadiusText.TabIndex = 0;
+            this.lblRadiusText.Text = "Radius:";
+            // 
+            // lblHeightText
+            // 
+            this.lblHeightText.AutoSize = true;
+            this.lblHeightText.Font = new System.Drawing.Font("Microsoft Sans Serif", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblHeightText.Location = new System.Drawing.Point(131, 153);
+            this.lblHeightText.Name = "lblHeightText";
+            this.lblHeightText.Size = new System.Drawing.Size(70, 24);
+            this.lblHeightText.TabIndex = 1;
+            this.lblHeightText.Text = "Height:";
+            // 
+            // txtRadius
+            // 
+            this.txtRadius.Location = new System.Drawing.Point(207, 83);
+            this.txtRadius.Name = "txtRadius";
+            this.txtRadius.Size = new System.Drawing.Size(100, 20);
+            this.txtRadius.TabIndex = 2;
+            // 
+            // txtHeight
+            // 
+            this.txtHeight.Location = new System.Drawing.Point(207, 158);
+            this.txtHeight.Name = "txtHeight";
+            this.txtHeight.Size = new System.Drawing.Size(100, 20);
+            this.txtHeight.TabIndex = 3;
+            // 
+            // btnCalculate
+            // 
+            this.btnCalculate.Font = new System.Drawing.Font("Microsoft Sans Serif", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnCalculate.Location = new System.Drawing.Point(188, 240);
+            this.btnCalculate.Name = "btnCalculate";
+            this.btnCalculate.Size = new System.Drawing.Size(119, 79);
+            this.btnCalculate.TabIndex = 4;
+            this.btnCalculate.Text = "Calculate Volume";
+            this.btnCalculate.UseVisualStyleBackColor = true;
+            this.btnCalculate.Click += new System.EventHandler(this.BtnCalculate_Click);
+            // 
+            // lblAnswer
+            // 
+            this.lblAnswer.AutoSize = true;
+            this.lblAnswer.Font = new System.Drawing.Font("Microsoft Sans Serif", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblAnswer.Location = new System.Drawing.Point(131, 367);
+            this.lblAnswer.Name = "lblAnswer";
+            this.lblAnswer.Size = new System.Drawing.Size(139, 24);
+            this.lblAnswer.TabIndex = 5;
+            this.lblAnswer.Text = "The volume is: ";
+            // 
+            // frmVolOfCylinder
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(507, 450);
+            this.Controls.Add(this.lblAnswer);
+            this.Controls.Add(this.btnCalculate);
+            this.Controls.Add(this.txtHeight);
+            this.Controls.Add(this.txtRadius);
+            this.Controls.Add(this.lblHeightText);
+            this.Controls.Add(this.lblRadiusText);
+            this.Name = "frmVolOfCylinder";
+            this.Text = "Volume of Cylinder by Ethan";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label lblRadiusText;
+        private System.Windows.Forms.Label lblHeightText;
+        private System.Windows.Forms.TextBox txtRadius;
+        private System.Windows.Forms.TextBox txtHeight;
+        private System.Windows.Forms.Button btnCalculate;
+        private System.Windows.Forms.Label lblAnswer;
+    }
+}
+
---
+++ VolumeOfCylinderEthanL/VolumeOfCylinderEthanL/VolumeOfCylinderForm.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace VolumeOfCylinderEthanL
+{
+    public partial class frmVolOfCylinder : Form
+    {
+        public frmVolOfCylinder()
+        {
+            InitializeComponent();
+        }
+
+        // Function: CalculateVolCylinder
+        // Input: double radius, double height
+        // Output: volume
+        // Description: This function returns volume with given the radius and height
+        private double CalculateVolCylinder(double radius, double height)
+        {
+
+        }
+        private void BtnCalculate_Click(object sender, EventArgs e)
+        {
+            // declare local variables
+            double userRadius;
+            double userHeight;
+            double userVolume;
+
+            // get the radius from the user
+            userRadius = Convert.ToInt16(this.txtRadius.Text);
+            userHeight = Convert.ToInt16(this.txtHeight.Text);
+
+            // call the function to return a random number
+            userVolume = CalculateVolCylinder();
+
+            // display the volume on the label
+            this.lblAnswer.Text = "The volume is: " + userVolume + "cubic meters";
+        }
+    }
+}
