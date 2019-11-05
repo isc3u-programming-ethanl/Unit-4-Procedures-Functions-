cb11bdc3db4e4f175725f7dfec418ab9d513dd7b
Ethan LeGault <47784129+s256202@users.noreply.github.com>
2019-11-05 17:17:42 -0500

    Updated VolOfCylinder

--- VolumeOfCylinderEthanL/VolumeOfCylinderEthanL/VolumeOfCylinderForm.cs
+++ VolumeOfCylinderEthanL/VolumeOfCylinderEthanL/VolumeOfCylinderForm.cs
@@ -1,4 +1,11 @@
-﻿using System;
+﻿/*
+ * Created by: Ethan L
+ * Created on: 11/05/19
+ * Created for: ICS3U Programming
+ * Daily Assignment – Day #30 - Volume of a Cylinder
+ * This program gets the users radius and height, and then calculates them and displays the volume of a cylinder (using the 2 inputs)
+*/
+using System;
 using System.Collections.Generic;
 using System.ComponentModel;
 using System.Data;
@@ -21,15 +28,19 @@ namespace VolumeOfCylinderEthanL
         // Input: double radius, double height
         // Output: volume
         // Description: This function returns volume with given the radius and height
-        private double CalculateVolCylinder(double radius, double height)
+        private double CalculateVolCylinder(int radius, int height)
         {
+            double userVolume; 
 
+            userVolume = 3.14 * Math.Pow(radius, 2) * height;
+
+            return userVolume;
         }
         private void BtnCalculate_Click(object sender, EventArgs e)
         {
             // declare local variables
-            double userRadius;
-            double userHeight;
+            int userRadius;
+            int userHeight;
             double userVolume;
 
             // get the radius from the user
@@ -37,10 +48,18 @@ namespace VolumeOfCylinderEthanL
             userHeight = Convert.ToInt16(this.txtHeight.Text);
 
             // call the function to return a random number
-            userVolume = CalculateVolCylinder();
+            userVolume = CalculateVolCylinder(userRadius, userHeight);
 
             // display the volume on the label
-            this.lblAnswer.Text = "The volume is: " + userVolume + "cubic meters";
+            this.lblAnswer.Text = "The volume is: " + userVolume + " cubic meters";
+
+            // show the label
+            this.lblAnswer.Show();
+        }
+
+        private void FrmVolOfCylinder_Load(object sender, EventArgs e)
+        {
+            this.lblAnswer.Hide();
         }
     }
 }
