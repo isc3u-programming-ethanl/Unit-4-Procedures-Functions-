44a2e8d349c2ab00a207c64666f3abaa802205c7
Ethan LeGault <47784129+s256202@users.noreply.github.com>
2019-11-11 13:04:33 -0500

    p

--- PercentageProgramEthanL/PercentageProgramEthanL/PercentageProgramForm.cs
+++ PercentageProgramEthanL/PercentageProgramEthanL/PercentageProgramForm.cs
@@ -38,71 +38,98 @@ namespace PercentageProgramEthanL
             {
                 case "4++":
                     percentage = 98;
+                    MessageBox.Show("Level " + level + " is equivalent to " + percentage + "%");
                     break;
                 case "4+":
                     percentage = 95;
+                    MessageBox.Show("Level " + level + " is equivalent to " + percentage + "%");
                     break;
                 case "4":
                     percentage = 87;
+                    MessageBox.Show("Level " + level + " is equivalent to " + percentage + "%");
                     break;
                 case "4-":
                     percentage = 83;
+                    MessageBox.Show("Level " + level + " is equivalent to " + percentage + "%");
                     break;
                 case "3+":
                     percentage = 78;
+                    MessageBox.Show("Level " + level + " is equivalent to " + percentage + "%");
                     break;
                 case "3":
                     percentage = 75;
+                    MessageBox.Show("Level " + level + " is equivalent to " + percentage + "%");
                     break;
                 case "3-":
                     percentage = 72;
+                    MessageBox.Show("Level " + level + " is equivalent to " + percentage + "%");
                     break;
                 case "2+":
                     percentage = 68;
+                    MessageBox.Show("Level " + level + " is equivalent to " + percentage + "%");
                     break;
                 case "2":
                     percentage = 65;
+                    MessageBox.Show("Level " + level + " is equivalent to " + percentage + "%");
                     break;
                 case "2-":
                     percentage = 62;
+                    MessageBox.Show("Level " + level + " is equivalent to " + percentage + "%");
                     break;
                 case "1+":
                     percentage = 58;
+                    MessageBox.Show("Level " + level + " is equivalent to " + percentage + "%");
                     break;
                 case "1":
                     percentage = 55;
+                    MessageBox.Show("Level " + level + " is equivalent to " + percentage + "%");
                     break;
                 case "1-":
                     percentage = 52;
+                    MessageBox.Show("Level " + level + " is equivalent to " + percentage + "%");
                     break;
                 case "0+":
                     percentage = 45;
+                    MessageBox.Show("Level " + level + " is equivalent to " + percentage + "%");
                     break;
                 case "0":
                     percentage = 40;
+                    MessageBox.Show("Level " + level + " is equivalent to " + percentage + "%");
                     break;
                 case "0-":
                     percentage = 35;
+                    MessageBox.Show("Level " + level + " is equivalent to " + percentage + "%");
                     break;
                 case "0--":
                     percentage = 30;
+                    MessageBox.Show("Level " + level + " is equivalent to " + percentage + "%");
                     break;
                 case "0---":
                     percentage = 20;
+                    MessageBox.Show("Level " + level + " is equivalent to " + percentage + "%");
                     break;
                 case "0----":
                     percentage = 10;
+                    MessageBox.Show("Level " + level + " is equivalent to " + percentage + "%");
                     break;
 
                 default:
                     percentage = -1;
-                        break;
+                    MessageBox.Show("Please enter a valid level");
+                    break;
             }
             return percentage;
         }
         private void BtnEnter_Click(object sender, EventArgs e)
         {
+            // declare variables
+            string userLevel;
+            int userPercentage;
 
+            userLevel = this.txtInput.Text;
+
+            // call the function
+            userPercentage = ConvertToPercent(userLevel);
         }
     }
 }
